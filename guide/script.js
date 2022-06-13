bbec282014168ad652fde08f18c25c584cf70914
iskanderqq <iskander.qq@mail.ru>
2022-06-13 14:11:56 +0300

    Update script.js

---
+++ guide/script.js
@@ -0,0 +1,6 @@
+document.querySelector('#dp-flex-button-1').onclick = function(ev){
+    document.querySelector('.show-container').style.display = 'block'
+}
+document.querySelector('#dp-flex-button-2').onclick = function(ev){
+    document.querySelector('.show-container').style.display = 'flex'
+}
