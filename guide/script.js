6eef54d90d35281c6800050c4ac405ba64143776
iskanderqq <iskander.qq@mail.ru>
2022-06-17 17:03:27 +0300

    Update script.js

--- guide/script.js
+++ guide/script.js
@@ -49,3 +49,21 @@ document.querySelector('#dp-flex-button-4_2').onclick = function(ev){
     document.querySelector('#dp-flex-button-3_2').classList.remove('active')
     document.querySelector('#dp-flex-button-1_2').classList.remove('active')
 }
+
+// 
+
+document.querySelector('#dp-flex-button-1_3').onclick = function(ev){
+    let a =document.querySelectorAll('.show-container')
+    a[2].style.display = 'flex'
+    a[2].style.flexWrap = 'nowrap'
+}
+document.querySelector('#dp-flex-button-2_3').onclick = function(ev){
+    let a =document.querySelectorAll('.show-container')
+    a[2].style.display = 'flex'
+    a[2].style.flexWrap = 'wrap'
+}
+document.querySelector('#dp-flex-button-3_3').onclick = function(ev){
+    let a =document.querySelectorAll('.show-container')
+    a[2].style.display = 'flex'
+    a[2].style.flexWrap = 'wrap-reverse'
+}
