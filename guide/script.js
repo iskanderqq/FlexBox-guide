c4b8eef73a23306ed0fce0aabcf4745227981e61
iskanderqq <iskander.qq@mail.ru>
2022-06-17 18:41:54 +0300

    Update script.js

--- guide/script.js
+++ guide/script.js
@@ -56,14 +56,23 @@ document.querySelector('#dp-flex-button-1_3').onclick = function(ev){
     let a =document.querySelectorAll('.show-container')
     a[2].style.display = 'flex'
     a[2].style.flexWrap = 'nowrap'
+    document.querySelector('#dp-flex-button-1_3').classList.add('active')
+    document.querySelector('#dp-flex-button-2_3').classList.remove('active')
+    document.querySelector('#dp-flex-button-3_3').classList.remove('active')
 }
 document.querySelector('#dp-flex-button-2_3').onclick = function(ev){
     let a =document.querySelectorAll('.show-container')
     a[2].style.display = 'flex'
     a[2].style.flexWrap = 'wrap'
+    document.querySelector('#dp-flex-button-1_3').classList.remove('active')
+    document.querySelector('#dp-flex-button-2_3').classList.add('active')
+    document.querySelector('#dp-flex-button-3_3').classList.remove('active')
 }
 document.querySelector('#dp-flex-button-3_3').onclick = function(ev){
     let a =document.querySelectorAll('.show-container')
     a[2].style.display = 'flex'
     a[2].style.flexWrap = 'wrap-reverse'
+    document.querySelector('#dp-flex-button-1_3').classList.remove('active')
+    document.querySelector('#dp-flex-button-2_3').classList.remove('active')
+    document.querySelector('#dp-flex-button-3_3').classList.add('active')
 }
